818cc8abf57ad1c32fbfc8685fbf25955bdffb5d
Pedroh1510 <47982475+Pedroh1510@users.noreply.github.com>
2025-02-26 19:39:32 -0300

    feat: add `diskusscan`

--- src/infra/engines/connectors/DiskusScan.mjs
+++ src/infra/engines/connectors/DiskusScan.mjs
@@ -1,13 +1,82 @@
-import WordPressMangastream from './templates/WordPressMangastream.mjs';
+import Connector from '../engine/Connector.mjs';
 
-export default class DiskusScan extends WordPressMangastream {
+export default class DiskusScan extends Connector {
+	constructor() {
+		super();
+		super.id = 'diskusscan';
+		super.label = 'Diskus Scan';
+		this.tags = ['manga', 'webtoon', 'portuguese', 'scanlation'];
+		this.url = 'https://diskusscan.online';
+		this.retries = 5;
+	}
+	async _getMangas() {
+		let page = 1;
+		let morePages = true;
 
-    constructor() {
-        super();
-        super.id = 'diskusscan';
-        super.label = 'Diskus Scan';
-        this.tags = [ 'manga', 'webtoon', 'portuguese', 'scanlation' ];
-        this.url = 'https://diskusscan.com';
-        this.path = '/manga/list-mode';
-    }
+		let request, data;
+
+		let mangas = [];
+
+		while (morePages) {
+			try {
+				request = new Request(
+					new URL('/page/' + page, this.url),
+					this.requestOptions
+				);
+				data = await this.fetchDOM(
+					request,
+					'div.stylefiv > div > div > div > a',
+					this.retries
+				);
+				page++;
+
+				for (let manga of data) {
+					const a = {
+						id: manga?.pathname,
+						title: manga?.textContent
+							?.trim()
+							?.replace(/\s+\d{2}(?: Final)?$/, '')
+							?.trim()
+					};
+					mangas.push(a);
+				}
+				if (!data?.length) morePages = false;
+			} catch (error) {
+				morePages = false;
+			}
+		}
+
+		return mangas;
+	}
+
+	async _getChapters(manga) {
+		let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+		let data = await this.fetchDOM(
+			request,
+			'#chapterlist > ul:nth-child(1) > li > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)',
+			this.retries
+		);
+
+		return data.map((element) => {
+			return {
+				id: element?.href?.replace(this.url, ''),
+				title: 'Chapter ' + element.querySelector('span')?.textContent,
+				language: 'pt'
+			};
+		});
+	}
+
+	async _getPages(chapter) {
+		let request = new Request(
+			new URL(chapter.id, this.url),
+			this.requestOptions
+		);
+		let data = await this.fetchDOM(request, 'body');
+		return data[0].outerHTML
+			.split('\n')
+			.filter((item) => item.includes('data-large-file='))
+			.map((item) =>
+				item.split('"').find((element) => element.includes(this.url))
+			);
+	}
 }
