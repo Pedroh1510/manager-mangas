3811bad2f866cd76dbcba07f920816e0729050e2
Pedro Silva <47982475+Pedroh1510@users.noreply.github.com>
2026-08-25 19:24:40 -0300

    refactor(manager-mangas): rewrite admin validators for idManga-based routes

--- src/validators/mangasAdmValidator.js
+++ src/validators/mangasAdmValidator.js
@@ -1,14 +1,72 @@
 import Joi from 'joi';
 
-async function registerManga({ body }, _, next) {
+async function createManga({ body }, _, next) {
 	const schema = Joi.object().keys({
 		title: Joi.string().required(),
-		titlePlugin: Joi.string().optional(),
-		idPlugin: Joi.string().required(),
 	});
 	await schema.validateAsync(body);
 	return next();
 }
+
+async function linkConnector(req, _, next) {
+	const schema = Joi.object().keys({
+		params: Joi.object().keys({
+			idManga: Joi.number().integer().required(),
+		}),
+		body: Joi.object().keys({
+			idPlugin: Joi.string().required(),
+			idMangaPlugin: Joi.string().required(),
+			titlePlugin: Joi.string().required(),
+		}),
+	});
+	await schema.validateAsync(req, { allowUnknown: true });
+	return next();
+}
+
+async function setConnectorActive(req, _, next) {
+	const schema = Joi.object().keys({
+		params: Joi.object().keys({
+			idManga: Joi.number().integer().required(),
+			idPlugin: Joi.string().required(),
+		}),
+		body: Joi.object().keys({
+			isActive: Joi.boolean().required(),
+		}),
+	});
+	await schema.validateAsync(req, { allowUnknown: true });
+	return next();
+}
+
+async function idMangaParam({ params }, _, next) {
+	const schema = Joi.object().keys({
+		idManga: Joi.number().integer().required(),
+	});
+	await schema.validateAsync(params);
+	return next();
+}
+
+async function chapterParams({ params }, _, next) {
+	const schema = Joi.object().keys({
+		idManga: Joi.number().integer().required(),
+		idChapter: Joi.number().integer().required(),
+	});
+	await schema.validateAsync(params);
+	return next();
+}
+
+async function downloadManga(req, _, next) {
+	const schema = Joi.object().keys({
+		params: Joi.object().keys({
+			idManga: Joi.number().integer().required(),
+		}),
+		query: Joi.object().keys({
+			volume: Joi.string().optional(),
+		}),
+	});
+	await schema.validateAsync(req, { allowUnknown: true });
+	return next();
+}
+
 async function listMangasRegistered({ query }, _, next) {
 	const schema = Joi.object().keys({
 		title: Joi.string().optional(),
@@ -16,6 +74,7 @@ async function listMangasRegistered({ query }, _, next) {
 	await schema.validateAsync(query);
 	return next();
 }
+
 async function registerCookie(req, _, next) {
 	const schema = Joi.object().keys({
 		body: Joi.object().keys({
@@ -26,6 +85,7 @@ async function registerCookie(req, _, next) {
 	await schema.validateAsync(req, { allowUnknown: true });
 	return next();
 }
+
 async function registerCredentials(req, _, next) {
 	const schema = Joi.object().keys({
 		body: Joi.object().keys({
@@ -37,42 +97,16 @@ async function registerCredentials(req, _, next) {
 	await schema.validateAsync(req, { allowUnknown: true });
 	return next();
 }
-async function listPagesAndSend({ query }, _, next) {
-	const schema = Joi.object().keys({
-		idChapterPlugin: Joi.string().required(),
-		pluginId: Joi.string().required(),
-		title: Joi.string().required(),
-		idChapter: Joi.string().required(),
-		volume: Joi.string().required(),
-	});
-	await schema.validateAsync(query);
-	return next();
-}
-async function updateMangaChapters({ query }, _, next) {
-	const schema = Joi.object().keys({
-		title: Joi.alternatives(
-			Joi.string(),
-			Joi.array().items(Joi.string()),
-		).required(),
-	});
-	await schema.validateAsync(query);
-	return next();
-}
-async function deleteMangaChapters({ query }, _, next) {
-	const schema = Joi.object().keys({
-		title: Joi.string().required(),
-		volume: Joi.string().required(),
-	});
-	await schema.validateAsync(query);
-	return next();
-}
+
 const MangasAdmValidator = {
-	registerManga,
+	createManga,
+	linkConnector,
+	setConnectorActive,
+	idMangaParam,
+	chapterParams,
+	downloadManga,
 	listMangasRegistered,
 	registerCookie,
 	registerCredentials,
-	listPagesAndSend,
-	updateMangaChapters,
-	deleteMangaChapters,
 };
 export default MangasAdmValidator;
